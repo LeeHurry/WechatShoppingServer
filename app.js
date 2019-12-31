a626704524bdac68b0df522e85332ce7f44d9c3c
李浩然06951 <haoran.li@ly.com>
2019-12-31 10:19:06 +0800

    新增详情页

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 //app.js
 App({
+  
   onLaunch: function() {
     console.log('App Launch')
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    var that = this;
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      success(res) {
+        if (res.code) {
+          console.log(res);
+          //发起网络请求
+          wx.request({
+            url: that.globalData.requestUrl +'Login/GetOpenId?code='+res.code,
+            data: {
+              code: res.code
+            },
+            success(res) {
+              console.log(res.data);
+              that.globalData.openid = res.data;
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
       }
     })
     // 获取用户信息
@@ -22,6 +38,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
+              console.info(res.userInfo);
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
@@ -35,11 +52,12 @@ App({
     })
   },
   globalData: {
-    userInfo: null,
+    userInfo:null,
+    openid:"",
     //requestUrl:"http://10.102.142.201:5005/api/",
     //imageUrl: "http://10.102.142.201:5005/",
-    requestUrl: "http://122.51.72.85:1001/api/",
-    imageUrl: "http://122.51.72.85:1001/",
+    requestUrl: "https://www.kikiestore.com/api/",
+    imageUrl: "https://www.kikiestore.com/",
     //requestUrl: "http://localhost:5000/api/",
   },
 
--- pages/main/main.js
+++ pages/main/main.js
@@ -6,43 +6,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list:[],
+    list: null,
     url: app.globalData.imageUrl
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.onloadData();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     this.onloadData();
   },
-
+  showDetail: function(e) {
+    console.log(e.currentTarget.id);
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + e.currentTarget.id,
+    })
+  },
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.onloadData();
   },
-  onloadData:function(){
+  onloadData: function() {
     var that = this;
     wx.request({
       //项目的真正接口，通过字符串拼接方式实现
-      url: app.globalData.requestUrl + "Main/GetCommodityList",
+      url: app.globalData.requestUrl + "Main/GetMainCommodityList",
       method: 'GET',
-      success: function (res) {
+      success: function(res) {
         console.log(res.data);
         that.setData({
           list: res.data
         });
       },
-      fail: function (e) {
+      fail: function(e) {
         console.log(e)
       },
     });
@@ -50,35 +55,35 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
     console.log('App 4')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
